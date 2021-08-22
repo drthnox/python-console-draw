2c07a22816d07f0e720a6b7b39870fd02b4f4802
DavidG <dg_codemonkey@outlook.com>
2021-08-22 09:15:31 +0800

    Updated rendering

--- ConsoleDraw/Canvas.py
+++ ConsoleDraw/Canvas.py
@@ -1,4 +1,32 @@
 class Canvas():
-    def __init__(self, rows, cols):
+
+    def __init__(self, rows=1, columns=1):
         self.rows = rows
-        self.columns = cols
+        self.columns = columns
+
+    def contains_point(self, row, col):
+        print(str(self.rows))
+        print(str(self.columns))
+        print("checking " + str(row) + ":" + str(col))
+        incols = True if (col >= 0 and col < self.columns) else False
+        print("incols="+str(incols))
+        inrows = True if (row >= 0 and row < self.rows) else False
+        print("inrows="+str(inrows))
+        result = incols and inrows
+        print("result: " + str(result))
+        return result
+
+    def render(self):
+        s = ""
+        for row in range(0, self.rows):
+            print("row:"+str(row))
+            for col in range(0, self.columns):
+                s += "+-"
+            s += "+\n"
+            for col in range(0, self.columns):
+                s += "| "
+            s += "|\n"
+        for col in range(0, self.columns):
+            s += "+-"
+        s += "+\n"
+        return s
--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -1,6 +1,19 @@
 from ConsoleDraw.Canvas import Canvas
 
+
 def test_create():
-    canvas = Canvas(10, 20)
+    canvas = Canvas(rows=10, columns=20)
     assert canvas.rows == 10
     assert canvas.columns == 20
+
+
+def test_contains_point():
+    canvas = Canvas(10, 20)
+    assert canvas.contains_point(1, 1) == True
+    assert canvas.contains_point(11, 1) == False
+    assert canvas.contains_point(1, 21) == False
+
+
+def test_render():
+    canvas = Canvas(3, 3)
+    assert canvas.render() == "+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n"
