b8c44afd06c867e258825d48c8ba7c5b53c3adc8
DavidG <dg_codemonkey@outlook.com>
2021-08-22 09:31:40 +0800

    Updated

--- ConsoleDraw/Canvas.py
+++ ConsoleDraw/Canvas.py
@@ -5,21 +5,14 @@ class Canvas():
         self.columns = columns
 
     def contains_point(self, row, col):
-        print(str(self.rows))
-        print(str(self.columns))
-        print("checking " + str(row) + ":" + str(col))
         incols = True if (col >= 0 and col < self.columns) else False
-        print("incols="+str(incols))
         inrows = True if (row >= 0 and row < self.rows) else False
-        print("inrows="+str(inrows))
         result = incols and inrows
-        print("result: " + str(result))
         return result
 
     def render(self):
         s = ""
         for row in range(0, self.rows):
-            print("row:"+str(row))
             for col in range(0, self.columns):
                 s += "+-"
             s += "+\n"
